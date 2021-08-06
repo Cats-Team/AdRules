20c14f5e035fb36a39be48e506f99a12008f9eb5
Iori-yimaga <964302865@qq.com>
2021-08-06 11:38:48 +0800

    重构修改yml

---
+++ AdGuardRules/mio.bat
@@ -0,0 +1,46 @@
+cd /d %~dp0
+cd aa
+@echo off
+
+::start download
+
+wget -O i1.txt https://filters.adtidy.org/android/filters/2_optimized.txt
+wget -O i2.txt https://filters.adtidy.org/android/filters/11_optimized.txt
+wget -O i3.txt https://filters.adtidy.org/android/filters/3_optimized.txt
+wget -O i4.txt https://filters.adtidy.org/android/filters/224_optimized.txt
+wget -O i5.txt https://filters.adtidy.org/android/filters/14_optimized.txt
+wget -O i6.txt https://filters.adtidy.org/android/filters/5_optimized.txt
+wget -O i7.txt https://filters.adtidy.org/android/filters/4_optimized.txt
+
+::add new rules
+::wget -O i+number url
+
+::end download
+
+::no need to change
+del /f /q *.html
+del /f /q *hsts
+
+::TheRuleMaker
+::No need to change
+
+::Merge
+type frules.dd>mergd.txt
+type i*.txt>>mergd.txt
+
+::nore
+gawk "!a[$0]++" mergd.txt>nore.txt
+
+::del comments
+(FOR /F "eol=# delims=" %%i in (nore.txt) do (echo %%i))>ktmp.txt
+(FOR /F "eol=[ delims=" %%i in (ktmp.txt) do (echo %%i))>stmp.txt
+(FOR /F "eol=! delims=" %%i in (stmp.txt) do (echo %%i))>nord.txt
+
+::add title and date
+echo ! Version: %date%>>tpdate.txt
+echo ! Last modified: %date%T%time%Z>>tpdate.txt
+copy title.dd+tpdate.txt+nord.txt+brules.dd final.txt
+
+::end
+copy /y final.txt ..\..\AdRules.txt
+del /f /q *.txt&exit
--- RMaker/mio.bat
+++ RMaker/mio.bat
@@ -18,13 +18,6 @@ wget -O i11.txt https://paulgb.github.io/BarbBlock/blacklists/hosts-file.txt
 wget -O i12.txt https://anti-ad.net/adguard.txt
 wget -O i13.txt https://gitee.com/xinggsf/Adblock-Rule/raw/master/rule.txt
 
-wget -O i14.txt https://filters.adtidy.org/android/filters/2_optimized.txt
-wget -O i15.txt https://filters.adtidy.org/android/filters/11_optimized.txt
-wget -O i16.txt https://filters.adtidy.org/android/filters/3_optimized.txt
-wget -O i17.txt https://filters.adtidy.org/android/filters/224_optimized.txt
-wget -O i18.txt https://filters.adtidy.org/android/filters/14_optimized.txt
-wget -O i19.txt https://filters.adtidy.org/android/filters/5_optimized.txt
-wget -O i20.txt https://filters.adtidy.org/android/filters/4_optimized.txt
 
 ::add new rules
 ::wget -O i+number url
@@ -56,5 +49,5 @@ echo ! Last modified: %date%T%time%Z>>tpdate.txt
 copy title.dd+tpdate.txt+nord.txt+brules.dd final.txt
 
 ::end
-copy /y final.txt ..\..\AdRules.txt
+copy /y final.txt ..\..\AdKillRules.txt
 del /f /q *.txt&exit
