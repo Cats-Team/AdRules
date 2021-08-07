6207cc847402614d1c459989268971df869e0d57
JokerDev <33000237+Iori-yimaga@users.noreply.github.com>
2021-08-07 12:49:45 +0800

    Update pusher.py

--- pusher.py
+++ pusher.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-token = ${{ secrets.PP_KEY }}
+token = 'c2e9e551adbc46029d98b06ec0e1c77c'
 title = 'AdRules'
 content = 'AdRules规则更新完毕，来自github~'
 url = 'http://pushplus.hxtrip.com/send'
