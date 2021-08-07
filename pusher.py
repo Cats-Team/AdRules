b86526685eeffb74271e55d38ad31319ecfbf5f2
Iori-yimaga <964302865@qq.com>
2021-08-07 22:51:06 +0800

    change the script

--- pusher.py
+++ pusher.py
@@ -1,9 +1,20 @@
 import requests
 import json
 
+adguard = r"./adguard.txt"
+AdKillRules = r"./AdKillRules.txt"
+count_a = -1
+for count_a, line in enumerate(open(adguard, 'r').readlines()):
+	count_a += 1
+	
+count_A = -1
+for count_A, line in enumerate(open(AdKillRules, 'r').readlines()):
+	count_A += 1
+
 token = 'c2e9e551adbc46029d98b06ec0e1c77c'
 title = 'AdRules'
-content = 'AdRules规则更新完毕，来自Github~，更新日期Updated at $(date)'
+content = 'AdRules规则更新完毕，来自Github~ <br>adguard.txt共计' + str(count_a) + '条规则，<br>AdKillRules' + str(count_A) + '条规则。'
+
 url = 'http://pushplus.hxtrip.com/send'
 data = {
 		"token":token,
