27b5b9010b11c2ce0ee0935ede8925bf8995ab2e
JokerDev <33000237+Iori-yimaga@users.noreply.github.com>
2021-08-08 09:37:22 +0800

    Update pusher.py

--- pusher.py
+++ pusher.py
@@ -16,8 +16,8 @@ def iter_count(file_name):
         buf_gen = takewhile(lambda x: x, (f.read(buffer) for _ in repeat(None)))
         return sum(buf.count('\n') for buf in buf_gen)
 
-count_a = iter_count(adguard)
-count_A = iter_count(AdKillRules)
+count_a = iter_count("adguard.txt")
+count_A = iter_count("AdKillRules.txt")
 token = 'c2e9e551adbc46029d98b06ec0e1c77c'
 title = 'AdRules'
 content = 'AdRules规则更新完毕，来自Github~ <br>adguard.txt共计' + str(count_a) + '条规则，<br>AdKillRules' + str(count_A) + '条规则。'
