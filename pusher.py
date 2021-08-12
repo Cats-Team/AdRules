2dc931a31f110da330e9ac29674a257e7f866884
Iori-yimaga <964302865@qq.com>
2021-08-12 09:26:12 +0800

    use env.Value instead of token

--- pusher.py
+++ pusher.py
@@ -9,18 +9,20 @@ def iter_count(file_name):
         buf_gen = takewhile(lambda x: x, (f.read(buffer) for _ in repeat(None)))
         return sum(buf.count('\n') for buf in buf_gen)
 
-count_a = iter_count("adguard.txt")
-count_A = iter_count("AdKillRules.txt")
-count_d = iter_count("dns.txt")
-token = 'c2e9e551adbc46029d98b06ec0e1c77c'
-title = 'AdRules'
-content = 'AdRules规则更新完毕，来自Github~<br>adguard.txt共计' + str(count_a) + '条规则，<br>AdKillRules共计' + str(count_A) + '条规则，<br>dns.txt共计' + str(count_d) + '条规则。'
+count_a = iter_count("adguard.txt") -8
+count_al = iter_count("allow.txt") -8
+count_A = iter_count("AdKillRules.txt") -8
+count_d = iter_count("dns.txt") -8
+
+PUSH_TOKEN = env.PUSH_TOKEN
+TITLE = 'AdRules'
+CONTENT = 'AdRules规则更新完毕，来自Github~<br>allow.txt共计' + str(count_al) + '条规则，<br>adguard.txt共计' + str(count_a) + '条规则，<br>AdKillRules共计' + str(count_A) + '条规则，<br>dns.txt共计' + str(count_d) + '条规则。'
 
 url = 'http://pushplus.hxtrip.com/send'
 data = {
-		"token":token,
-		"title":title,
-		"content":content
+		"token":PUSH_TOKEN,
+		"title":TITLE,
+		"content":CONTENT
 }
 body = json.dumps(data).encode(encoding = 'utf-8')
 headers = {'Content-Type':'application/json'}
