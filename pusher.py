757ca2bb19561c0c4d976623250b46d50ee7da48
Iori-yimaga <964302865@qq.com>
2021-08-12 13:37:52 +0800

    update

--- pusher.py
+++ pusher.py
@@ -3,6 +3,13 @@ import requests
 import json
 import os
 
+
+PUSH_TOKEN = os.environ["SCKEY"]
+TG_TOKEN = os.environ["TG_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+CONTENT = 'AdRules规则更新完毕，来自Github~<br>allow.txt共计' + str(count_al) + '条规则，<br>adguard.txt共计' + str(count_a) + '条规则，<br>AdKillRules共计' + str(count_A) + '条规则，<br>dns.txt共计' + str(count_d) + '条规则。'
+
 def iter_count(file_name):
     from itertools import (takewhile, repeat)
     buffer = 1024 * 1024
@@ -19,33 +26,31 @@ def post_tg(message):
         ('disable_web_page_preview', "yes")
     )
     telegram_url = "https://api.telegram.org/bot" + TG_TOKEN + "/sendMessage"
-    telegram_req = post(telegram_url, params=params)
+    telegram_req = requests.post(telegram_url, params=params)
     telegram_status = telegram_req.status_code
     if telegram_status == 200:
         print(f"INFO: Telegram Message sent")
     else:
         print("Telegram Error")
 
-count_a = iter_count("adguard.txt") -8
-count_al = iter_count("allow.txt") -8
-count_A = iter_count("AdKillRules.txt") -8
-count_d = iter_count("dns.txt") -8
+def post_pp():
+    count_a = iter_count("adguard.txt") -8
+    count_al = iter_count("allow.txt") -8
+    count_A = iter_count("AdKillRules.txt") -8
+    count_d = iter_count("dns.txt") -8
 
-PUSH_TOKEN = os.environ["SCKEY"]
-TG_TOKEN = os.environ["TG_TOKEN"]
-CHAT_ID = os.environ["CHAT_ID"]
+    TITLE = 'AdRules'
+    CONTENT = 'AdRules规则更新完毕，来自Github~<br>allow.txt共计' + str(count_al) + '条规则，<br>adguard.txt共计' + str(count_a) + '条规则，<br>AdKillRules共计' + str(count_A) + '条规则，<br>dns.txt共计' + str(count_d) + '条规则。'
 
-TITLE = 'AdRules'
-CONTENT = 'AdRules规则更新完毕，来自Github~<br>allow.txt共计' + str(count_al) + '条规则，<br>adguard.txt共计' + str(count_a) + '条规则，<br>AdKillRules共计' + str(count_A) + '条规则，<br>dns.txt共计' + str(count_d) + '条规则。'
-
-url = 'http://pushplus.hxtrip.com/send'
-data = {
-		"token":PUSH_TOKEN,
-		"title":TITLE,
-		"content":CONTENT
-}
-body = json.dumps(data).encode(encoding = 'utf-8')
-headers = {'Content-Type':'application/json'}
-requests.post(url, data=body, headers=headers)
+    url = 'http://pushplus.hxtrip.com/send'
+    data = {
+		  "token":PUSH_TOKEN,
+		  "title":TITLE,
+		  "content":CONTENT
+        }
+    body = json.dumps(data).encode(encoding = 'utf-8')
+    headers = {'Content-Type':'application/json'}
+    requests.post(url, data=body, headers=headers)
 
 post_tg(CONTENT)
+post_pp()
