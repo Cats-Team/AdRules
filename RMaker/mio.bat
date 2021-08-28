d4b80c1382ddbd0caa76eb985acad3e4a5d82547
杏梢 <73057402+hacamer@users.noreply.github.com>
2021-08-28 13:37:32 +0800

    去除重复规则

--- RMaker/mio.bat
+++ RMaker/mio.bat
@@ -10,15 +10,14 @@ wget -O i3.txt https://raw.githubusercontent.com/banbendalao/ADgk/master/ADgk.tx
 wget -O i4.txt https://file.trli.club/dns/hosts.txt
 wget -O i5.txt https://raw.githubusercontent.com/banbendalao/ADgk/master/kill-baidu-ad.txt
 wget -O i6.txt https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/rule.txt
-wget -O i7.txt https://easylist-downloads.adblockplus.org/antiadblockfilters.txt
-wget -O i8.txt https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock.txt
-wget -O i9.txt https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock_lite.txt
-wget -O i10.txt https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock_plus.txt
-wget -O i11.txt https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock_privacy.txt
-wget -O i12.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/annoyances.txt
-wget -O i13.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/badware.txt
-wget -O i14.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters.txt
-wget -O i15.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/privacy.txt
+wget -O i7.txt https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock.txt
+wget -O i8.txt https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock_lite.txt
+wget -O i9.txt https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock_plus.txt
+wget -O i10.txt https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock_privacy.txt
+wget -O i11.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/annoyances.txt
+wget -O i12.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/badware.txt
+wget -O i13.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters.txt
+wget -O i14.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/privacy.txt
 
 
 ::add new rules
