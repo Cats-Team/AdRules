6be7f14a11aa840f70f027fe69b38e208c3b80f5
杏梢 <73057402+hacamer@users.noreply.github.com>
2021-12-19 17:33:34 +0800

    Update main.sh

--- script/adblock+adguard/main.sh
+++ script/adblock+adguard/main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd script/adblock/src
+cd script/adblock+adguard/src
 
 # Start Download
 curl -o i1.txt https://raw.githubusercontent.com/o0HalfLife0o/list/master/ad-pc.txt
@@ -9,14 +9,24 @@ curl -o i4.txt https://raw.githubusercontent.com/banbendalao/ADgk/master/kill-ba
 curl -o i5.txt https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/rule.txt
 curl -o i6.txt https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock.txt
 curl -o i7.txt https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock_lite.txt
-#curl -o i9.txt https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock_plus.txt
+curl -o i9.txt https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock_plus.txt
 curl -o i8.txt https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock_privacy.txt
 curl -o i11.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/annoyances.txt
 curl -o i12.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/badware.txt
-#curl -o i13.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters.txt
+curl -o i13.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters.txt
 curl -o i14.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/privacy.txt
 curl -o i15.txt https://raw.githubusercontent.com/Noyllopa/NoAppDownload/master/NoAppDownload.txt
 curl -o i16.txt https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt
+curl -o i17.txt https://filters.adtidy.org/android/filters/2_optimized.txt
+curl -o i18.txt https://filters.adtidy.org/android/filters/11_optimized.txt
+curl -o i19.txt https://filters.adtidy.org/android/filters/3_optimized.txt
+curl -o i20.txt https://filters.adtidy.org/android/filters/224_optimized.txt
+curl -o i21.txt https://filters.adtidy.org/android/filters/14_optimized.txt
+curl -o i22.txt https://filters.adtidy.org/android/filters/5_optimized.txt
+curl -o i23.txt https://filters.adtidy.org/android/filters/4_optimized.txt
+curl -o i24.txt https://raw.githubusercontent.com/Cats-Team/AdRule/main/url-filter.txt
+curl -o i25.txt https://raw.githubusercontent.com/Cats-Team/AdRule/main/rules-admin.txt
+curl -o i26.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/script/adblock/src/user-rules.dd
 
 # Start Merge and Duplicate Removal
 cat i*.txt > mergd.txt
