81a300d179540c3b498934393e807d99979b26df
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-01-18 09:21:12 +0800

    Update main.sh

--- script/dns/main.sh
+++ script/dns/main.sh
@@ -4,7 +4,7 @@ cd script/dns/src
 
 # Start Download
 curl -o i1.txt https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
-curl -o i4.txt https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts
+#curl -o i4.txt https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts
 #curl -o i5.txt https://raw.githubusercontent.com/badmojr/1Hosts/master/mini/adblock.txt
 curl -o i6.txt https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/rule.txt
 curl -o i7.txt https://raw.githubusercontent.com/o0HalfLife0o/list/master/ad-pc.txt
@@ -17,8 +17,8 @@ cat i*.txt > mergd.txt
 cat mergd.txt | grep '^|' | grep -v './' | grep -v '.\$' > block.txt
 cat mergd.txt | grep '^@' | grep -v './' | grep -v '.\$' > allow.txt
 cat mergd.txt | grep '^/' | grep -v './' | grep -v '.+'| grep -v '.-' | grep -v '.&' | grep -v '._' | grep -v '.?' | grep -v '.x'| grep -v '.\=' | grep -v '.[A-Z]'|grep -v '.\$' | grep -v '.js'| grep -v '.png' | grep -v '.^' | grep -v '.\*'| grep -v '.\|' | grep -v '.~'> pu.txt
-cat mergd.txt | grep '^[0-9]' > hosts.txt
-cat block.txt allow.txt pu.txt hosts.txt > new.txt
+#cat mergd.txt | grep '^[0-9]' > hosts.txt
+cat block.txt allow.txt pu.txt > new.txt
 cat new.txt | grep -v '.#' | grep -v '.?' | grep -v '.=' | grep -v '.]'> tmpp.txt
 sort -n tmpp.txt | uniq > tmp.txt
 
