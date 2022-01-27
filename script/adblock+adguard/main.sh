3872b66fe60ffe80a2b820fe7cf0f02de566eb66
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-01-27 23:51:18 +0800

    Update main.sh

--- script/adblock+adguard/main.sh
+++ script/adblock+adguard/main.sh
@@ -17,13 +17,13 @@ curl -o i13.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/fi
 curl -o i14.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/privacy.txt
 curl -o i15.txt https://raw.githubusercontent.com/Noyllopa/NoAppDownload/master/NoAppDownload.txt
 curl -o i16.txt https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt
-curl -o i17.txt https://filters.adtidy.org/android/filters/2_optimized.txt
-curl -o i18.txt https://filters.adtidy.org/android/filters/11_optimized.txt
-curl -o i19.txt https://filters.adtidy.org/android/filters/3_optimized.txt
-curl -o i20.txt https://filters.adtidy.org/android/filters/224_optimized.txt
-curl -o i21.txt https://filters.adtidy.org/android/filters/14_optimized.txt
-curl -o i22.txt https://filters.adtidy.org/android/filters/5_optimized.txt
-curl -o i23.txt https://filters.adtidy.org/android/filters/4_optimized.txt
+curl -o i17.txt https://filters.adtidy.org/android/filters/2.txt
+curl -o i18.txt https://filters.adtidy.org/android/filters/11.txt
+curl -o i19.txt https://filters.adtidy.org/android/filters/3.txt
+curl -o i20.txt https://filters.adtidy.org/android/filters/224.txt
+curl -o i21.txt https://filters.adtidy.org/android/filters/14.txt
+curl -o i22.txt https://filters.adtidy.org/android/filters/5.txt
+curl -o i23.txt https://filters.adtidy.org/android/filters/4.txt
 curl -o i24.txt https://raw.githubusercontent.com/Cats-Team/AdRule/main/url-filter.txt
 curl -o i25.txt https://raw.githubusercontent.com/Cats-Team/AdRule/main/rules-admin.txt
 curl -o i26.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/script/adblock/src/user-rules.dd
@@ -31,9 +31,8 @@ curl -o i26.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/script/
 # Start Merge and Duplicate Removal
 cat i*.txt > mergd.txt
 cat mergd.txt | grep -v '^!' | grep -v '^！' | grep -v '^# ' | grep -v '^# ' | grep -v '^\[' | grep -v '^\【' > tmpp.txt
-iconv -f GBK -t UTF-8 tmpp.txt > cmp.txt
-sort -n cmp.txt | uniq > tmpt.txt
-iconv -f UTF-8 -t GBK tmpt.txt > tmp.txt
+sort -n tmpp.txt | uniq | awk '!a[$0]++' > tmp.txt
+
 
 
 # Start Count Rules
