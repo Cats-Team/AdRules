bb6d476e7abba218e023c1ab4c2a33125c46feb7
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-02-07 12:03:49 +0800

    Update main.sh

--- script/dns/main.sh
+++ script/dns/main.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 cd script/dns/src
 #cd ./src
-rm -f fu.dd
 # Start Download
 curl -o i1.txt https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
-#curl -o i4.txt https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts
-#curl -o i5.txt https://raw.githubusercontent.com/badmojr/1Hosts/master/mini/adblock.txt
 curl -o i6.txt https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/rule.txt
 curl -o i7.txt https://raw.githubusercontent.com/o0HalfLife0o/list/master/ad-pc.txt
 curl -o i8.txt https://raw.githubusercontent.com/o0HalfLife0o/list/master/ad-edentw.txt
 curl -o i9.txt https://raw.githubusercontent.com/banbendalao/ADgk/master/ADgk.txt
 curl -o i10.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/adblock.txt
+curl -o i11.txt https://adaway.org/hosts.txt
 
 # Start Merge and Duplicate Removal
 cat i*.txt > mergd.txt
@@ -33,8 +31,6 @@ echo "! Total count: $num" >> tpdate.txt
 cat title.dd tpdate.txt tmp.txt > final.txt
 
 mv final.txt ../../dns.txt
-mv pu.txt fu.dd
 rm *.txt
-rm dns*.*
 cd ../../
 exit
