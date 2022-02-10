bd457081e894d023ed99ff42f2abbb674ce9c1d0
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-02-10 08:29:24 +0800

    Update main.sh

--- script/hosts/main.sh
+++ script/hosts/main.sh
@@ -7,25 +7,31 @@ curl -o i2.txt https://raw.githubusercontent.com/r-a-y/mobile-hosts/master/Adgua
 curl -o i3.txt https://adaway.org/hosts.txt
 curl -o i4.txt https://raw.githubusercontent.com/r-a-y/mobile-hosts/master/AdguardApps.txt
 curl -o i5.txt https://raw.githubusercontent.com/r-a-y/mobile-hosts/master/AdguardMobileAds.txt
-curl -o i6.txt https://raw.githubusercontent.com/badmojr/1Hosts/master/mini/hosts.txt
+#curl -o i6.txt https://raw.githubusercontent.com/badmojr/1Hosts/master/mini/hosts.txt
+wget https://raw.githubusercontent.com/Cats-Team/AdRules/main/dns.txt
+cat dns.txt | grep '^|' | grep -v '\*'| grep -Ev "([0-9]{1,3}.){3}[0-9]{1,3}" |sed 's/||/0.0.0.0 /' | sed 's/\^//' | grep -v "^|" > hosts.txt
 
 # Start Merge and Duplicate Removal
 cat i*.txt > mergd.txt
 cat mergd.txt | grep '^1'  > 1.txt
 cat mergd.txt | grep '^0'  > 0.txt
-cat 1.txt 0.txt > tmpp.txt
+cat 1.txt 0.txt hosts.txt > tmpp.txt
 sort tmpp.txt | uniq > tmp.txt
 
+# Create ad damian
+cat tmp.txt | sed 's/0.0.0.0 //' > damian-init.txt
 
 # Start Count Rules
 num=`cat tmp.txt | wc -l`
 
 # Start Add title and date
-echo "! Version: `date +"%Y-%m-%d %H:%M:%S"`" >> tpdate.txt
-echo "! Total count: $num" >> tpdate.txt
+echo "# Version: `date +"%Y-%m-%d %H:%M:%S"`" >> tpdate.txt
+echo "# Total count: $num" >> tpdate.txt
 cat title.dd tpdate.txt tmp.txt > final.txt
+cat damian-init.txt tpdate.txt >> damian.txt
 
 mv final.txt ../../hosts.txt
+mv damian.txt ../../../damian.txt
 rm *.txt
 cd ../../
 exit
