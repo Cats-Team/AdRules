3a23597263c4f2824af475f93a95cc319685bdc6
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-02-15 17:46:05 +0800

    Update pusher.py

--- pusher.py
+++ pusher.py
@@ -26,10 +26,11 @@ count_damian = iter_count("damian.txt") -2
 #设置时区
 tz_utc_8 = timezone(timedelta(hours=8))
 #设置时间
-now = datetime.now()
+time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 
-CONTENT = 'AdRules规则更新完毕！ 来自Github~<br>Allowlist共计' + str(count_al) + '条规则，<br>AdRules (For AdGuard)共计' + str(count_a) + '条规则，<br>AdRules (For AdBlock)共计' + str(count_A) + '条规则，<br>AdRules (For DNS)共计' + str(count_d) + '条规则，<br>AdRules (For Adaway)共计' + str(count_ad) + '条规则，<br>广告damian共计' + str(count_damian) + '个。<br>更新时间' + str(now.strftime('%Y-%m-%d %H:%M:%S')) '（北京时间）'
+
+CONTENT = 'AdRules规则更新完毕！ 来自Github~<br>Allowlist共计' + str(count_al) + '条规则，<br>AdRules (For AdGuard)共计' + str(count_a) + '条规则，<br>AdRules (For AdBlock)共计' + str(count_A) + '条规则，<br>AdRules (For DNS)共计' + str(count_d) + '条规则，<br>AdRules (For Adaway)共计' + str(count_ad) + '条规则，<br>广告damian共计' + str(count_damian) + '个。<br>更新时间' + str(time) '（北京时间）'
 
 #def post_tg(message):
 #    telegram_message = f"{message}"
