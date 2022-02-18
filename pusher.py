70ef41f602fce977afa03fcef0682215acc26d76
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-02-18 23:20:02 +0800

    Update pusher.py

--- pusher.py
+++ pusher.py
@@ -41,7 +41,7 @@ one = requests.get(api_url)
 
 #图库
 png_url = 'https://api.ixiaowai.cn/api/api.php'
-png = requests.get(url)
+png = requests.get(png_url)
 
 
 CONTENT = 'AdRules规则更新完毕！ 来自Github~<br>更新时间 ' + str(time) + '（北京时间）<br><br>AdRules (For AdBlock)共计' + str(count_A) + '条规则，<br>AdRules (For AdGuard)共计' + str(count_a) + '条规则，<br>AdRules (For DNS)共计' + str(count_d) + '条规则，<br>AdRules (For Adaway)共计' + str(count_ad) + '条规则，<br>Allowlist共计' + str(count_al) + '条规则，<br>广告damian共计' + str(count_damian) + '个。<br><br>一言：' + str(one.text) +' <img src="' + str(png) + '"/>' 
