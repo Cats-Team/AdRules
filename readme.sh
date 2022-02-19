cb24bb4dcc333ffeba9f13204779d6ca6549e249
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-02-19 15:39:18 +0800

    Fix

--- readme.sh
+++ readme.sh
@@ -11,10 +11,10 @@ declare -i count_adb=$num_adb-$num
 declare -i count_dns=$num_dns-$num
 declare -i count_hosts=$num_hosts-$num
 declare -i count_damian=$num_damian-2
-sed -i 's/^更新时间:.*/更新时间: $(TZ=UTC-8 date +'%Y-%m-%d %H:%M:%S')（北京时间） /g' README.md
-sed -i 's/^AdRules（For Adblock）规则数量:.*/AdRules（For Adblock）规则数量: $count_adb /g' README.md
-sed -i 's/^AdRules（For AdGuard）规则数量:.*/AdRules（For AdGuard）规则数量: $count_adg /g' README.md
-sed -i 's/^AdRules（For DNS）规则数量:.*/AdRules（For DNS）规则数量: $count_dns /g' README.md
-sed -i 's/^AdRules（For adaway）规则数量:.*/AdRules（For adaway）规则数量: $count_hosts /g' README.md
-sed -i 's/^Allowlist规则数量:.*/Allowlist规则数量: $count_hosts /g' README.md
-sed -i 's/^Damian数量:.*/Damian数量: $count_damian /g' README.md
+sed -i 's/^更新时间:.*/更新时间: '$(TZ=UTC-8 date +'%Y-%m-%d %H:%M:%S')（北京时间）' /g' README.md
+sed -i 's/^AdRules（For Adblock）规则数量:.*/AdRules（For Adblock）规则数量: '$count_adb' /g' README.md
+sed -i 's/^AdRules（For AdGuard）规则数量:.*/AdRules（For AdGuard）规则数量: '$count_adg' /g' README.md
+sed -i 's/^AdRules（For DNS）规则数量:.*/AdRules（For DNS）规则数量: '$count_dns' /g' README.md
+sed -i 's/^AdRules（For adaway）规则数量:.*/AdRules（For adaway）规则数量: '$count_hosts' /g' README.md
+sed -i 's/^Allowlist规则数量:.*/Allowlist规则数量: '$count_hosts' /g' README.md
+sed -i 's/^Damian数量:.*/Damian数量: '$count_damian' /g' README.md
