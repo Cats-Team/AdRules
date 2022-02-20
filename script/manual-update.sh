9e0011c3ce22e3a59ce60077e39e1ec85d92e339
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-02-20 13:34:56 +0800

    Update manual-update.sh

--- script/manual-update.sh
+++ script/manual-update.sh
@@ -1,4 +1,4 @@
-curl -X POST https://api.github.com/repos/cats-team/AdRule/dispatches \
+curl -X POST https://api.github.com/repos/hacamer/adblock_list/dispatches \
     -H "Accept: application/vnd.github.v3+json" \
     -H "Authorization: token $GITHUBTOKEN" \
     --data '{"event_type": "Manual-Update"}'
