d807daf9b96a3713e3da38e2eb6a4a1e2ebecdb5
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-02-20 12:54:32 +0800

    Update manual-push.sh

--- script/manual-push.sh
+++ script/manual-push.sh
@@ -1,4 +1,4 @@
-curl -X POST https://api.github.com/repos/cats-team/AdRules/dispatches \
+curl -X POST https://api.github.com/repos/cats-team/AdRule/dispatches \
     -H "Accept: application/vnd.github.v3+json" \
     -H "Authorization: token $GITHUBTOKEN" \
     --data '{"event_type": "Manual-Update"}'
