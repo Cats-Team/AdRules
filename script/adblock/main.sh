7b3d58e1a4bcd6d8344004e7adb35168d1f5a759
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-03-06 23:28:43 +0800

    basic

--- script/adblock/main.sh
+++ script/adblock/main.sh
@@ -13,24 +13,15 @@ easylist=(
   "https://raw.githubusercontent.com/cjx82630/cjxlist/master/cjxlist.txt"
   "https://easylist-downloads.adblockplus.org/antiadblockfilters.txt"
   "https://easylist-downloads.adblockplus.org/easyprivacy.txt"
+  "https://raw.githubusercontent.com/hacamer/Adblist/master/ad-pc.txt"
+  "https://raw.githubusercontent.com/banbendalao/ADgk/master/ADgk.txt"
+  "https://raw.githubusercontent.com/banbendalao/ADgk/master/kill-baidu-ad.txt"
+  "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/LegitimateURLShortener.txt"
+  "https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/mv.txt"
+  "https://raw.githubusercontent.com/Noyllopa/NoAppDownload/master/NoAppDownload.txt"
+  "https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt"
+  "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/ClearURLs%20for%20uBo/clear_urls_uboified.txt"
 )
-
-curl -o i1.txt https://raw.githubusercontent.com/hacamer/Adblist/master/ad-pc.txt
-curl -o i3.txt https://raw.githubusercontent.com/banbendalao/ADgk/master/ADgk.txt
-curl -o i4.txt https://raw.githubusercontent.com/banbendalao/ADgk/master/kill-baidu-ad.txt
-curl -o i5.txt https://raw.githubusercontent.com/DandelionSprout/adfilt/master/LegitimateURLShortener.txt
-curl -o i6.txt https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/mv.txt
-#curl -o i6.txt https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock.txt
-#curl -o i7.txt https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock_lite.txt
-#curl -o i9.txt https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock_plus.txt
-#curl -o i8.txt https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock_privacy.txt
-curl -o i11.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/annoyances.txt
-curl -o i12.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/badware.txt
-curl -o i13.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters.txt
-curl -o i14.txt https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/privacy.txt
-curl -o i15.txt https://raw.githubusercontent.com/Noyllopa/NoAppDownload/master/NoAppDownload.txt
-curl -o i16.txt https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt
-curl -o i17.txt https://raw.githubusercontent.com/DandelionSprout/adfilt/master/ClearURLs%20for%20uBo/clear_urls_uboified.txt
 
 for i in "${!easylist[@]}"
 do
@@ -42,11 +33,10 @@ do
     exit 1
   fi
 done
-
 # Start Merge and Duplicate Removal
-cat i*.txt user-rules.dd easy*.txt > mergd.txt
+cat user-rules.dd easy*.txt > mergd.txt
 cat mergd.txt | grep -v '^!' | grep -v '^！' | grep -v '^# ' | grep -v '^# ' | grep -v '^\[' | grep -v '^\【' | grep -v 'local.adguard.org' > tmpp.txt
-sort -n tmpp.txt | uniq > tmmp.txt
+sort -n tmpp.txt | uniq -u > tmmp.txt
 
 # Remove Error Rules
 cat tmmp.txt err-rules.dd > ttmp.txt 
