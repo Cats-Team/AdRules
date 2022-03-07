5d5dca025e401aade937bb8526bf93db338b56c2
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-03-07 21:19:39 +0800

    Update main.sh

--- script/hosts/main.sh
+++ script/hosts/main.sh
@@ -2,13 +2,9 @@
 cd script/hosts/src
 
 # Start Download
-curl -o i1.txt https://raw.githubusercontent.com/r-a-y/mobile-hosts/master/AdguardDNS.txt 
-curl -o i2.txt https://raw.githubusercontent.com/r-a-y/mobile-hosts/master/AdguardMobileSpyware.txt
-curl -o i3.txt https://adaway.org/hosts.txt
-curl -o i4.txt https://raw.githubusercontent.com/r-a-y/mobile-hosts/master/AdguardApps.txt
-curl -o i5.txt https://raw.githubusercontent.com/r-a-y/mobile-hosts/master/AdguardMobileAds.txt
-#curl -o i6.txt https://raw.githubusercontent.com/badmojr/1Hosts/master/mini/hosts.txt
-wget https://raw.githubusercontent.com/Cats-Team/AdRules/main/dns.txt
+echo "开始更新AdRules（For Adaway）"
+#wget https://raw.githubusercontent.com/Cats-Team/AdRules/main/dns.txt
+cp ../../dns.txt ./dns.txt
 cat dns.txt | grep '^|' | grep -v '\*'| grep -Ev "([0-9]{1,3}.){3}[0-9]{1,3}" |sed 's/||/0.0.0.0 /' | sed 's/\^//' | grep -v "^|" > hosts.txt
 
 # Start Merge and Duplicate Removal
@@ -18,7 +14,7 @@ cat mergd.txt | grep '^0'  > 0.txt
 cat 1.txt 0.txt hosts.txt > tmpp.txt
 cat tmpp.txt | grep -v 'local' | sed 's/127.0.0.1 /0.0.0.0 /' >temp.txt
 sort -n temp.txt | uniq > tmp.txt
-
+echo "开始处理规则"
 # Create ad damian
 cat tmp.txt | sed 's/0.0.0.0 //' > damian-init.txt
 
@@ -30,7 +26,7 @@ echo "# Version: `date +"%Y-%m-%d %H:%M:%S"`" >> tpdate.txt
 echo "# Total count: $num" >> tpdate.txt
 cat title.dd tpdate.txt tmp.txt > final.txt
 cat tpdate.txt damian-init.txt >> damian.txt
-
+echo "规则处理完毕"
 mv final.txt ../../hosts.txt
 mv damian.txt ../../../damian.txt
 rm *.txt
