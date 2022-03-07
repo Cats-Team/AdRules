035b6e3bbdc2bf16ac6eb772ee0e4c89cc851472
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-03-07 21:17:37 +0800

    Update main.sh

--- script/allow/main.sh
+++ script/allow/main.sh
@@ -2,17 +2,17 @@
 cd script/allow/src
 
 # Start Download
-curl -o i1.txt https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
-curl -o i2.txt https://raw.githubusercontent.com/Potterli20/hosts/main/ad-allow.txt
-curl -o i3.txt https://adaway.org/hosts.txt
-curl -o i4.txt https://raw.githubusercontent.com/banbendalao/ADgk/master/ADgk.txt
-curl -o i5.txt https://raw.githubusercontent.com/o0HalfLife0o/list/master/ad-pc.txt
-curl -o i6.txt https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/master/ChineseFilter/sections/whitelist.txt
-curl -o i7.txt https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/master/GermanFilter/sections/whitelist.txt 
-curl -o i8.txt https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/master/TurkishFilter/sections/whitelist.txt 
-curl -o i9.txt https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/master/SpywareFilter/sections/whitelist.txt
-curl -o damianlist1.txt https://raw.githubusercontent.com/privacy-protection-tools/dead-horse/master/anti-ad-white-list.txt
-#curl -o i10.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/rules/allow-lm.txt
+echo "开始更新Allowlist"
+wget -q -O i1.txt https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
+wget -q -O i2.txt https://raw.githubusercontent.com/Potterli20/hosts/main/ad-allow.txt
+wget -q -O i3.txt https://adaway.org/hosts.txt
+wget -q -O i4.txt https://raw.githubusercontent.com/banbendalao/ADgk/master/ADgk.txt
+wget -q -O i5.txt https://raw.githubusercontent.com/o0HalfLife0o/list/master/ad-pc.txt
+wget -q -O i6.txt https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/master/ChineseFilter/sections/whitelist.txt
+wget -q -O i7.txt https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/master/GermanFilter/sections/whitelist.txt 
+wget -q -O i8.txt https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/master/TurkishFilter/sections/whitelist.txt 
+wget -q -O i9.txt https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/master/SpywareFilter/sections/whitelist.txt
+wget -q -O damianlist1.txt https://raw.githubusercontent.com/privacy-protection-tools/dead-horse/master/anti-ad-white-list.txt
 
 # Start Merge and Duplicate Removal
 cat damianlist*.txt | sed "s/^/@@||&/g" |sed "s/$/&^/g" >> iw.txt
@@ -20,7 +20,7 @@ cat i*.txt > mergd.txt
 cat mergd.txt | grep '^@' > allow.txt
 cat allow.txt > tmpp.txt
 sort -n tmpp.txt | uniq > tmp.txt
-
+echo "开始处理规则"
 # Start Count Rules
 num=`cat tmp.txt | wc -l`
 
@@ -29,7 +29,7 @@ echo "! Title: Allowlist" >> tpdate.txt
 echo "! Version: $(TZ=UTC-8 date +'%Y-%m-%d %H:%M:%S')（北京时间） " >> tpdate.txt
 echo "! Total count: $num" >> tpdate.txt
 cat tpdate.txt frules.dd tmp.txt > final.txt
-
+echo "规则处理完毕"
 mv final.txt ../../allow.txt
 rm *.txt
 cd ../../
