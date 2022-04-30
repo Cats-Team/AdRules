4ad2d5aef11b191c7c1db9ebe903b63dc50e0070
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-04-30 23:16:15 +0800

    增加白名單

--- script/prepare-dns-upstream.sh
+++ script/prepare-dns-upstream.sh
@@ -10,6 +10,9 @@ dead_hosts=(
   "https://raw.githubusercontent.com/privacy-protection-tools/dead-horse/master/anti-ad-white-list.txt"
   "https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt"
   "https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/optional-list.txt"
+  "https://raw.githubusercontent.com/Ultimate-Hosts-Blacklist/whitelist/master/domains.list"
+  "https://raw.githubusercontent.com/neodevpro/neodevhost/master/customallowlist"
+  "https://raw.githubusercontent.com/EnergizedProtection/unblock/master/basic/formats/domains.txt"
 )
 for i in "${!dead_hosts[@]}"
 do
